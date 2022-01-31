ac46543fa2d3629da5b3a1abb33f801cf797bd15
clayton-costa <62947316+clayton-costa@users.noreply.github.com>
2022-01-31 17:42:05 -0300

    Atualizacao p/ funcionar a desgrama

--- modelo1_mod.py
+++ modelo1_mod.py
@@ -1,6 +1,6 @@
 # Carrega bibliotecas necessárias
 import tflite_runtime.interpreter as tflite
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import io
 
@@ -10,17 +10,7 @@ interpreter = tflite.Interpreter(model_path='modelo1.tflite')
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 
-################# ROUBADO DA TPU ##################
-def draw_objects(draw, bbox, label, score):
-  """Draws the bounding box and label for each object."""
-  for obj in objs:
-    draw.rectangle([(bbox[1], bbox[0]), (bbox[3], bbox[2])],
-                   outline='red')
-    draw.text((bbox[1] + 10, bbox[0] + 10),
-              label + ' ' + score,
-              fill='red')
 
-################# ROUBADO DA TPU ##################
 
 # Inicializa nomes dos labels
 labels = ['sc', 'cc']
@@ -28,7 +18,8 @@ labels = ['sc', 'cc']
 # Carrega uma imagem para teste e adequa
 img = Image.open('1.jpg')
 larg, alt = img.size
-img2.thumbnail((320,320), Image.ANTIALIAS)
+img2 = img
+img2 = img2.resize((320,320))
 img_cvt = np.array(img2.getdata()).reshape(img2.size[0], img2.size[1], 3)
 img_cvt = (img_cvt.astype('float32')/127.5)-1 # Limites entre -1 e 1
 img_cvt = np.expand_dims(img_cvt, axis=0) # Corrige p/ o tensor ([x, y, c] -> [1, x, y, c])
@@ -45,8 +36,9 @@ boxes = interpreter.get_tensor(output_details[1]['index'])[0]
 classes = interpreter.get_tensor(output_details[3]['index'])[0] 
 scores = interpreter.get_tensor(output_details[0]['index'])[0]
 
-
+draw = ImageDraw.Draw(img)
 for i in range(len(scores)):
+
     if ((scores[i] > 0.4) and (scores[i] <= 1.0)):
         # Busca coordenadas do objeto atual. É possível que as coordenadas retornadas estejam fora
         # dos limites da imagem, por isso é forçado com "max e min"
@@ -55,7 +47,16 @@ for i in range(len(scores)):
         ymax = int(min(alt,(boxes[i][2] * alt)))
         xmax = int(min(larg,(boxes[i][3] * larg)))
 
-        draw_object(img, [ymin, xmin, ymax, xmax], labels[int(classes[i])], scores[i])
+        if classes[i] == 0:
+            cor = 'red'
+        else:
+            cor = 'blue'
+
+        draw.rectangle([(xmin, ymin), (xmax, ymax)],
+            outline=cor)
+        draw.text((xmin + 10, ymin + 10),
+            labels[int(classes[i])] + ' ' + str(round(scores[i]*100, 2)) + '%',
+            font = ImageFont.truetype('arial.ttf', 16), fill=cor)
 
 
         
